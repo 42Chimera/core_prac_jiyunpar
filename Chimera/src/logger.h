3c329c09fc3b81adfe477d4c63be380e19f9f24e
flowjiyun <gyp2795@gmail.com>
2024-01-08 13:30:49 +0900

    format : 포멧 변경

--- Chimera/src/logger.h
+++ Chimera/src/logger.h
@@ -4,17 +4,18 @@
 #include "spdlog/spdlog.h"
 
 namespace Chimera {
-    class CHIMERA_API Logger {
-        public:
-            static void Init();
+class CHIMERA_API Logger {
+   public:
+    static void Init();
 
-            inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() {return sCoreLogger;}
-            inline static std::shared_ptr<spdlog::logger>& GetClientLogger() {return sClientLogger;}
-        private:
-            static std::shared_ptr<spdlog::logger> sCoreLogger;
-            static std::shared_ptr<spdlog::logger> sClientLogger;
-    };
-}
+    inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return sCoreLogger; }
+    inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return sClientLogger; }
+
+   private:
+    static std::shared_ptr<spdlog::logger> sCoreLogger;
+    static std::shared_ptr<spdlog::logger> sClientLogger;
+};
+}  // namespace Chimera
 
 // Core Logger Macro
 #define CM_CORE_TRACE(...) ::Chimera::Logger::GetCoreLogger()->trace(__VA_ARGS__)
@@ -25,7 +26,7 @@ namespace Chimera {
 
 // Client Logger Macro
 #define CM_CLIENT_TRACE(...) ::Chimera::Logger::GetClientLogger()->trace(__VA_ARGS__)
-#define CM_CLIENT_INFO(...)  ::Chimera::Logger::GetClientLogger()->info(__VA_ARGS__)
-#define CM_CLIENT_WARN(...)  ::Chimera::Logger::GetClientLogger()->warn(__VA_ARGS__)
+#define CM_CLIENT_INFO(...) ::Chimera::Logger::GetClientLogger()->info(__VA_ARGS__)
+#define CM_CLIENT_WARN(...) ::Chimera::Logger::GetClientLogger()->warn(__VA_ARGS__)
 #define CM_CLIENT_ERROR(...) ::Chimera::Logger::GetClientLogger()->error(__VA_ARGS__)
 #define CM_CLIENT_FATAL(...) ::Chimera::Logger::GetClientLogger()->fatal(__VA_ARGS__)
