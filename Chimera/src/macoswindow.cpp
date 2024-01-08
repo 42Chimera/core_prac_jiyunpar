430b9ee37bf876562f0b7197b684d1b58d3c4cb1
flowjiyun <gyp2795@gmail.com>
2024-01-08 20:57:08 +0900

    feat: macos용 윈도우 객체 구현

--- Chimera/src/macoswindow.cpp
+++ Chimera/src/macoswindow.cpp
@@ -19,8 +19,35 @@ void MacOSWindow::Init(const WindowProps& props) {
     mData.Title = props.Title;
     mData.Width = props.Width;
     mData.Height = props.Height;
+
+    CM_CORE_INFO("Creating Window {0} ({1} {2})", props.Title, props.Width, props.Height);
+
+    if (!sGLFWInitialized) {
+        int success = glfwInit();
+    }
+
+    mWindow = glfwCreateWindow(static_cast<int>(props.Width), static_cast<int>(props.Height), props.Title.c_str(), nullptr, nullptr);
+    glfwMakeContextCurrent(mWindow);
+    glfwSetWindowUserPointer(mWindow, &mData);
+    SetVSync(true);
 }
 void MacOSWindow::Shutdown() {
     glfwDestroyWindow(mWindow);
+}
+
+void MacOSWindow::OnUpdate() {
+    glfwPollEvents();
+    glfwSwapBuffers(mWindow);
+}
+void MacOSWindow::SetVSync(bool enabled) {
+    if (enabled) {
+        glfwSwapInterval(1);
+    } else {
+        glfwSwapInterval(0);
+    }
+    mData.VSync = enabled;
+}
+bool MacOSWindow::IsVSync() const {
+    return mData.VSync;
 }
 }  // namespace Chimera
